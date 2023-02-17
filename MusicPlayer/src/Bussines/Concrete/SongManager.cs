8d135a6924b319d8cb18ce0d046e1707772eb5c8
huseyinekh <huseyinekh@CANR2-4>
2023-02-17 11:25:20 +0400

    add-project

---
+++ MusicPlayer/src/Bussines/Concrete/SongManager.cs
@@ -0,0 +1,25 @@
+﻿using AutoMapper;
+using Bussines.Abstract;
+using Data.DTO_s;
+using Data.Entities;
+using DataAccess.Abstract.UnitOfWork;
+
+namespace Bussines.Concrete;
+
+public class SongManager : ISongService
+{
+    private readonly IUnitOfWork _unitOfWork;
+    private readonly IMapper _mapper;
+    public SongManager(IUnitOfWork unitOfWork, IMapper mapper)
+    {
+        this._unitOfWork = unitOfWork;
+        _mapper = mapper;
+    }
+
+    public async Task Add(SongCreateDto songDto)
+    {
+        var result = _mapper.Map<Song>(songDto);
+        await _unitOfWork.SongRepository.Add(result);
+        await _unitOfWork.SaveChangesAsync();
+    }
+}
---
+++ MusicPlayer/src/Bussines/ServiceConfiguration.cs
@@ -0,0 +1,16 @@
+﻿using Bussines.Abstract;
+using Bussines.Concrete;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace Bussines;
+
+public static class ServiceConfiguration
+{
+    public static IServiceCollection AddBussines(this IServiceCollection services)
+    {
+        services.AddAutoMapper(Assembly.GetExecutingAssembly());
+        services.AddScoped<ISongService, SongManager>();
+        return services;
+    }
+}
